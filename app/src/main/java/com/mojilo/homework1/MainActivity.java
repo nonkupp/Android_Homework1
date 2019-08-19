7e95bb7e11d727ca455d650638b9dbe88ca66a18
mojilo <ctongkien@gmail.com>
2019-08-19 18:45:01 +0700

    few

--- app/src/main/java/com/mojilo/homework1/MainActivity.java
+++ app/src/main/java/com/mojilo/homework1/MainActivity.java
@@ -12,7 +12,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Toast t = Toast.makeText(MainActivity.this , "Hello", Toast.LENGTH_SHORT); //long_short 2-3sec long 4-5 sec
+        Toast t = Toast.makeText(MainActivity.this , "Hello", Toast.LENGTH_SHORT); //long_short 2-3sec long 4-5 se
         t.show();
         Toast t2 = Toast.makeText(MainActivity.this , "Android", Toast.LENGTH_SHORT); //long_short 2-3sec long 4-5 sec
         t2.show();
